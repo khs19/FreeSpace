da29c0058c331a2bfd8a6267c11b8884037e6a54
khs19 <vhkf19@gmail.com>
2021-06-06 16:03:56 +0900

    searchResultActivity 관련

--- app/src/main/java/com/example/freespace/SearchActivity.kt
+++ app/src/main/java/com/example/freespace/SearchActivity.kt
@@ -5,14 +5,17 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.freespace.databinding.ActivitySearchBinding
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import java.io.PrintStream
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SearchActivity : AppCompatActivity() {
     lateinit var binding: ActivitySearchBinding
     lateinit var layoutManager: LinearLayoutManager
-    lateinit var adapter: MyPlaceAdapter
+    lateinit var adapter: SearchAdapter
+    var recents = mutableListOf<String>()
+    var array = ArrayList<String>()
     lateinit var rdb: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,53 +23,53 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
         init()
     }
-
-
     private fun init() {
-        layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        rdb = FirebaseDatabase.getInstance().getReference("PlaceDB/Place")
-        val query = rdb.orderByKey()
-        val option =  FirebaseRecyclerOptions.Builder<Place>().setQuery(query, Place::class.java).build()
-        adapter = MyPlaceAdapter(option)
-        adapter.itemClickListener = object : MyPlaceAdapter.OnItemClickListener {
-            override fun OnItemClick(view: View, position: Int) {
-                val intent = Intent(this@SearchActivity, SaturationActivity::class.java)
-                intent.putExtra("string", adapter.getItem(position).pName.toString())
-                intent.putExtra("int", (adapter.getItem(position).pNum.toString().toDouble()/adapter.getItem(position).pMaxNum.toString().toDouble()*100).toInt())
-                startActivity(intent)
-            }
-        }
+        initData()
         binding.apply {
-            searchRecyclerView.layoutManager = layoutManager
-            searchRecyclerView.adapter = adapter
+            searchRecyclerView.layoutManager = LinearLayoutManager(parent,LinearLayoutManager.VERTICAL,false)
+//            var arrRecent = ArrayList<String>()
+//            recent(arrRecent)
+            adapter = SearchAdapter(array)
             searchBtn.setOnClickListener {
-//                val query = rdb.orderByChild("pname").equalTo(searchEdit.text.toString())//query문 찾아봐야함
-//                val option = FirebaseRecyclerOptions.Builder<Place>().setQuery(query, Place::class.java).build()
-//                adapter = MyPlaceAdapter(option)
-//                adapter.itemClickListener = object : MyPlaceAdapter.OnItemClickListener {
-//                    override fun OnItemClick(view: View, position: Int) {
-//                        val intent = Intent(this@SearchActivity, SaturationActivity::class.java)
-//                        intent.putExtra("string", adapter.getItem(position).pName.toString())
-//                        intent.putExtra("int", (adapter.getItem(position).pNum.toString().toDouble()/adapter.getItem(position).pMaxNum.toString().toDouble()*100).toInt())
-//                        startActivity(intent)
-//                    }
-//                }
-//                recyclerView.adapter = adapter
-//                adapter.startListening()
+                writefile(searchEdit.text.toString())
                 val intent = Intent(this@SearchActivity, SearchResultActivity::class.java)
-                intent.putExtra("placeName", searchEdit.text)
+                intent.putExtra("placeName", searchEdit.text.toString())
                 startActivity(intent)
             }
+            adapter.itemClickListener = object : SearchAdapter.OnItemClickListener{
+                override fun OnItemClick(holder: SearchAdapter.ViewHolder, view: View, data: String, position: Int) {
+                    searchEdit.setText(array[position])
+                }
+            }
+            searchRecyclerView.adapter = adapter
         }
     }
-
-    override fun onStart() {
-        super.onStart()
-        adapter.startListening()
+    fun readFileScan(scan: Scanner){
+        while(scan.hasNextLine()){
+            val name = scan.nextLine()
+            array.add(name)
+        }
+        scan.close()
     }
+    private fun initData() {
 
-    override fun onStop() {
-        super.onStop()
-        adapter.stopListening()
+        try { // addvoc를 하지 않아 out.txt가 생성되지 않았을경우 try catch
+            val scan2 = Scanner(this.openFileInput("recent.txt"))
+            readFileScan(scan2) // addvocactivity 에서 삽입한 텍스트를 삽입
+
+        }catch(e: Exception){ }
+
+    }
+    fun recent(items: ArrayList<String>){
+        items.clear()
+        var i = 0
+        while(i<10&&i<array.size){
+            items.add(array[i])
+        }
+    }
+    fun writefile(name: String){
+        val output= PrintStream(this.openFileOutput("recent.txt", MODE_APPEND))
+        output.println(name)
+        output.close()
     }
 }
--- app/src/main/java/com/example/freespace/SearchResultActivity.kt
+++ app/src/main/java/com/example/freespace/SearchResultActivity.kt
@@ -1,11 +1,60 @@
 package com.example.freespace
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.freespace.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.freespace.databinding.ActivitySearchResultBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SearchResultActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySearchResultBinding
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var adapter: MyPlaceAdapter
+    lateinit var rdb: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_result)
+        binding = ActivitySearchResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        rdb = FirebaseDatabase.getInstance().getReference("PlaceDB/Place")
+        val intent = intent
+        val placeName = intent.getStringExtra("placeName")
+
+        val query = rdb.orderByChild("pname").startAt(placeName).endAt("$placeName\uf8ff")
+        val option =  FirebaseRecyclerOptions.Builder<Place>().setQuery(query, Place::class.java).build()
+        adapter = MyPlaceAdapter(option)
+        adapter.itemClickListener = object : MyPlaceAdapter.OnItemClickListener {
+            override fun OnItemClick(view: View, position: Int) {
+                val intent = Intent(this@SearchResultActivity, SaturationActivity::class.java)
+                intent.putExtra("string", adapter.getItem(position).pName.toString())
+                intent.putExtra("int", (adapter.getItem(position).pNum.toString().toDouble()/adapter.getItem(position).pMaxNum.toString().toDouble()*100).toInt())
+                startActivity(intent)
+            }
+        }
+        binding.apply {
+            resultRecyclerView.layoutManager = layoutManager
+            resultRecyclerView.adapter = adapter
+            moveBookMark.setOnClickListener {
+                val intent = Intent(this@SearchResultActivity, BookmarkActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
     }
 }
